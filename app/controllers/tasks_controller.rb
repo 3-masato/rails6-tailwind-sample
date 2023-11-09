f6ca8d89344f44ee175d59c7348a55c39fe4d4e0
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-09 08:41:24 +0000

    Added feature to add and delete tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,37 @@
 class TasksController < ApplicationController
-  def new
-  end
-
   def edit
+    @task = Task.find(params[:id])
   end
 
   def show
   end
 
   def index
+    @tasks = Task.all
   end
-  
+
   def create
+    @task = Task.new(task_params)
+    @tasks = Task.all
+
+    if @task.save
+      redirect_to tasks_path, notice: "Success"
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task&.destroy
+    redirect_to tasks_path
+  end
+
+  def update
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title)
+  end
 end
